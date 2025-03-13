e760e6fdb5ef087216a5fac9238e4fe69f22eb00
NavodContrath <navod.contrath@gmail.com>
2025-03-13 13:07:58 +0100

    add require to import posts routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const postsRouter = require('./routers/posts')
 
 //listening to server
 app.listen(port, () => {
     console.log(`server starte in local host: http://localhost:${port}`)
 })
+
