4f3ac815d1f188983bc33b4bb79c837fab6b6410
NavodContrath <navod.contrath@gmail.com>
2025-03-13 13:08:26 +0100

    add middleware

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ const postsRouter = require('./routers/posts')
 app.listen(port, () => {
     console.log(`server starte in local host: http://localhost:${port}`)
 })
-
+//middleware
+app.use('/api/v1/posts', postsRouter)
